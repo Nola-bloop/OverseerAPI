8271eda6a9c31b13d6ea91c4dd6fb858bda4ebce
Nola <Nola.Bloop@gmail.com>
2026-01-18 19:07:10 -0500

    fast commit

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const PORT = 8889
 //routes
 import usersRouter from './routes/users.route.js'
 import clusterOutputRouter from './routes/clusterOutput.route.js'
+import clusterInputRouter from './routes/clusterInput.route.js'
 
 app.use("/users", usersRouter);
 app.use("/clusterOutput", clusterOutputRouter);
+app.use("/clusterInput", clusterInputRouter);
 
 app.get('/', (req, res) => {
   res.send('Empty route.')
