ab5b931c40f5ada142415938775a2240bee3c01d
Nola <Nola.Bloop@gmail.com>
2026-01-19 22:39:34 -0500

    fast commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import usersRouter from './routes/users.route.js'
 import clusterOutputRouter from './routes/clusterOutput.route.js'
 import clusterInputRouter from './routes/clusterInput.route.js'
 
+app.use(express.json());
 app.use("/users", usersRouter);
 app.use("/clusterOutput", clusterOutputRouter);
 app.use("/clusterInput", clusterInputRouter);
