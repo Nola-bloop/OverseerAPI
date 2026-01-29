cee1c056071c22793d7d8abc48876e7c05c6ee97
Nola <Nola.Bloop@gmail.com>
2026-01-29 13:24:22 -0500

    fast commit

--- models/clusterOutput.model.js
+++ models/clusterOutput.model.js
@@ -27,7 +27,7 @@ export default {
 	},
 	ReadMessagesByChapterId : async (id) => {
 		return new Promise((resolve, reject) =>{
-			con.query(`SELECT messages.* FROM messages WHERE messages.chapter = ? ORDER BY messages.date_sent, messages.id`, [id], (e, results) => {
+			con.query(`SELECT id, dc_message_id, chapter, speaker, date_sent, thread FROM messages WHERE messages.chapter = ? ORDER BY messages.date_sent, messages.id`, [id], (e, results) => {
 				if (!e) resolve(results)
 				else reject(e)
 			})
@@ -35,7 +35,7 @@ export default {
 	},
 	ReadMessageFromDiscordId: async (id) => {
 		return new Promise((resolve, reject) =>{
-			con.query(`SELECT id, dc_message_id, chapter, speaker, date_sent, thread FROM messages WHERE messages.dc_message_id = ?`,[id], (e, results)=>{
+			con.query(`SELECT messages.* FROM messages WHERE messages.dc_message_id = ?`,[id], (e, results)=>{
 				if (!e) resolve(results[0])
 				else reject(e)
 			})
